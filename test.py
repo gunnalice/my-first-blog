1b4543e2f4006e49308203e0554320c83d02a1a9
gunnalice <gatorgrimsen@gmail.com>
2018-03-24 15:13:29 +0100

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,20 @@
+def hi(name):
+    print('hi there!')
+    print('how are you?')
+
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous!')
+
+hi("gunn")
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
